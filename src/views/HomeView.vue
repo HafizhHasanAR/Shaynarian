93418d5d82d0136a80a42f003cb1a1f0338692b2
Hafizh Hasan Alauddin Rabbani <hafizhhafizh9@gmail.com>
2022-08-03 13:15:12 +0700

    Convert HTML to VUe2

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <BanerShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import BanerShayna from "@/components/BanerShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    BanerShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
+</script>
